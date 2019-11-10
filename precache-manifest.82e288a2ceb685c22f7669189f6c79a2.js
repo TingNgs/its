bb2275df8ec5ad8e08985b761f9ba9c410d7eb25
TingNgs <t105590049@ntut.org.tw>
2019-11-10 18:03:30 +0800

    Updates

---
+++ precache-manifest.82e288a2ceb685c22f7669189f6c79a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b7f3a593265db870454b8e02d1ae0b",
+    "url": "/its/index.html"
+  },
+  {
+    "revision": "30da82b78828c3a9f9e5",
+    "url": "/its/static/css/main.4058d15a.chunk.css"
+  },
+  {
+    "revision": "ce95e45db4e06fad81fb",
+    "url": "/its/static/js/2.37cad71e.chunk.js"
+  },
+  {
+    "revision": "30da82b78828c3a9f9e5",
+    "url": "/its/static/js/main.ab9fb940.chunk.js"
+  },
+  {
+    "revision": "e04cefe5881000a06da4",
+    "url": "/its/static/js/runtime-main.edc38807.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c70000ea1641f9e359e1bdc187c3979.js"
+  "/its/precache-manifest.82e288a2ceb685c22f7669189f6c79a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/its/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
