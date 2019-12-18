f570bee6153349006082e062cf578aba58b8481c
TingNgs <t105590049@ntut.org.tw>
2019-12-18 23:10:36 +0800

    Finish setting page function

--- src/components/IssueCard/index.js
+++ src/components/IssueCard/index.js
@@ -1,49 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { toLocalTime } from '../../utils/generalUtils';
-import { ISSUE_DETAIL_LINK } from '../../utils/pathConst';
-import './style.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import { toLocalTime } from "../../utils/generalUtils";
+import { ISSUE_DETAIL_LINK } from "../../utils/pathConst";
+import "./style.scss";
 import {
-    STATE_OPTION,
-    SEVERITY_OPTION,
-    PRIORITY_OPTION
-} from '../../utils/configConst';
+  STATE_OPTION,
+  SEVERITY_OPTION,
+  PRIORITY_OPTION
+} from "../../utils/configConst";
 const IssueCard = ({ issue }) => {
-    const {
-        id,
-        reportUser,
-        create_time,
-        description,
-        isReproducible,
-        name,
-        priority,
-        projectId,
-        severity,
-        state,
-        tags
-    } = issue;
+  const {
+    id,
+    reportUser,
+    create_time,
+    description,
+    isReproducible,
+    name,
+    priority,
+    projectId,
+    severity,
+    state,
+    tags
+  } = issue;
 
-    return (
-        <Link to={ISSUE_DETAIL_LINK(id)}>
-            <div className="IssueCard w-full">
-                <div className="IssueCard_title text-20">{name}</div>
-                <div>
-                    state : {STATE_OPTION[state]}, priority:
-                    {PRIORITY_OPTION[priority]}, severity:
-                    {SEVERITY_OPTION[severity]}
-                </div>
-                <div>create at : {toLocalTime(create_time)}</div>
-                <div>
-                    {tags.map(e => (
-                        <div className="IssueCard_Tags_container bg-green-200">
-                            {' '}
-                            {e}{' '}
-                        </div>
-                    ))}
-                </div>
+  return (
+    <Link to={ISSUE_DETAIL_LINK(id)}>
+      <div className="IssueCard w-full">
+        <div className="IssueCard_title text-20">{name}</div>
+        <div>
+          state : {STATE_OPTION[state]}, priority:
+          {PRIORITY_OPTION[priority]}, severity:
+          {SEVERITY_OPTION[severity]}
+        </div>
+        <div>create at : {toLocalTime(create_time)}</div>
+        <div>
+          {tags.map(e => (
+            <div
+              key={`issueCardtag${e}${id}`}
+              className="IssueCard_Tags_container bg-green-200"
+            >
+              {e}
             </div>
-        </Link>
-    );
+          ))}
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default IssueCard;
