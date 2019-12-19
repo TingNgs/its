307c04824a8135932b54f6d083195b9713e4090e
TingNgs <t105590049@ntut.org.tw>
2019-12-19 21:26:49 +0800

    Fix activity for assign

--- src/components/IssueActivity/index.js
+++ src/components/IssueActivity/index.js
@@ -1,45 +1,55 @@
-import React from 'react';
+import React from "react";
 
 import {
-    STATE_OPTION,
-    SEVERITY_OPTION,
-    PRIORITY_OPTION
-} from '../../utils/configConst';
+  STATE_OPTION,
+  SEVERITY_OPTION,
+  PRIORITY_OPTION
+} from "../../utils/configConst";
 
 const IssueActivity = ({ activity }) => {
-    console.log('Here is a issue activity', activity);
+  console.log("Here is a issue activity", activity);
 
-    const type = ['state', 'severity', 'priority', 'Reproducible'];
-    const typeValue = [
-        STATE_OPTION,
-        SEVERITY_OPTION,
-        PRIORITY_OPTION,
-        ['Yes', 'No']
-    ];
+  const type = ["state", "severity", "priority", "Reproducible"];
+  const typeValue = [
+    STATE_OPTION,
+    SEVERITY_OPTION,
+    PRIORITY_OPTION,
+    ["Yes", "No"]
+  ];
 
-    if (activity.type === 0) {
-        return (
-            <div className="issueActivity text-16 mt-3 mb-3">
-                {activity.username} commented:{' '}
-                <div dangerouslySetInnerHTML={{ __html: activity.content }} />
-            </div>
-        );
-    }
-    if (activity.prevState === -1) {
-        return (
-            <div className="issueActivity text-16 mt-3 mb-3">
-                {activity.username} start issue with {type[activity.type - 1]}{' '}
-                {typeValue[activity.type - 1][activity.nextState]}
-            </div>
-        );
-    }
+  if (activity.type === 0) {
     return (
-        <div className="issueActivity text-16 mt-3 mb-3">
-            {activity.username} changed {type[activity.type - 1]} from{' '}
-            {typeValue[activity.type - 1][activity.prevState]} to{' '}
-            {typeValue[activity.type - 1][activity.nextState]}
-        </div>
+      <div className="issueActivity text-16 mt-3 mb-3">
+        {activity.username} commented:{" "}
+        <div dangerouslySetInnerHTML={{ __html: activity.content }} />
+      </div>
     );
+  }
+  if (activity.type === 5) {
+    return (
+      <div className="issueActivity text-16 mt-3 mb-3">
+        {activity.username} assigned:
+        {activity.assignList.map(e => {
+          return e.isAdd ? <div>{e.username}</div> : null;
+        })}
+      </div>
+    );
+  }
+  if (activity.prevState === -1) {
+    return (
+      <div className="issueActivity text-16 mt-3 mb-3">
+        {activity.username} start issue with {type[activity.type - 1]}{" "}
+        {typeValue[activity.type - 1][activity.nextState]}
+      </div>
+    );
+  }
+  return (
+    <div className="issueActivity text-16 mt-3 mb-3">
+      {activity.username} changed {type[activity.type - 1]} from{" "}
+      {typeValue[activity.type - 1][activity.prevState]} to{" "}
+      {typeValue[activity.type - 1][activity.nextState]}
+    </div>
+  );
 };
 
 export default IssueActivity;
