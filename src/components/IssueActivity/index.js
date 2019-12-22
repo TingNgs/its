0ed13e8857695c55bf6702e617251b39c65871a7
hehe <thianhe@hotmail.com>
2019-12-22 16:39:06 +0800

    update issueDetail UI

--- src/components/IssueActivity/index.js
+++ src/components/IssueActivity/index.js
@@ -1,59 +1,79 @@
-import React from "react";
+import React from 'react';
 
+import PROFILE_IMAGE from '../../utils/image/user_icon.svg';
+import OPEN_BOX from '../../utils/image/open-box.svg';
 import {
-  STATE_OPTION,
-  SEVERITY_OPTION,
-  PRIORITY_OPTION
-} from "../../utils/configConst";
-
-const IssueActivity = ({ activity }) => {
-  const type = ["state", "severity", "priority", "Reproducible"];
-  const typeValue = [
     STATE_OPTION,
     SEVERITY_OPTION,
-    PRIORITY_OPTION,
-    ["Yes", "No"]
-  ];
+    PRIORITY_OPTION
+} from '../../utils/configConst';
+import './style.scss';
+const IssueActivity = ({ activity }) => {
+    const type = ['state', 'severity', 'priority', 'Reproducible'];
+    const typeValue = [
+        STATE_OPTION,
+        SEVERITY_OPTION,
+        PRIORITY_OPTION,
+        ['Yes', 'No']
+    ];
 
-  if (activity.type === 0) {
-    return (
-      <div className="issueActivity text-16 mt-3 mb-3">
-        {activity.username} commented:{" "}
-        <div dangerouslySetInnerHTML={{ __html: activity.content }} />
-      </div>
-    );
-  }
-  if (activity.type === 5) {
-    return (
-      <div className="issueActivity text-16 mt-3 mb-3">
-        {activity.username} assigned:
-        {activity.assignList.map((e, i) => {
-          return e.isAdd ? (
-            <div key={`issueActivity_assign${e}_${i}`}>{e.username}</div>
-          ) : (
-            <div key={`issueActivity_assign${e}_${i}`} className="line-through">
-              {e.username}
+    if (activity.type === 0) {
+        return (
+            <div className="issueActivity text-16 mt-3 mb-3">
+                {activity.username} commented:{' '}
+                <div dangerouslySetInnerHTML={{ __html: activity.content }} />
             </div>
-          );
-        })}
-      </div>
-    );
-  }
-  if (activity.prevState === -1) {
+        );
+    }
+    if (activity.type === 5) {
+        return (
+            <div className="issueActivity text-16 mt-3 mb-3">
+                {activity.username} assigned:
+                {activity.assignList.map((e, i) => {
+                    return e.isAdd ? (
+                        <div key={`issueActivity_assign${e}_${i}`}>
+                            {e.username}
+                        </div>
+                    ) : (
+                        <div
+                            key={`issueActivity_assign${e}_${i}`}
+                            className="line-through"
+                        >
+                            {e.username}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+    if (activity.prevState === -1) {
+        return (
+            <div className="issueActivity text-16 pl-10 mt-3 mb-3 flex text-center items-center">
+                <span className="">
+                    <div className="issueActivity_profileIcon flex  ">
+                        <img src={PROFILE_IMAGE} className="" />
+                    </div>
+                </span>
+                <div className="hl "></div>
+                <div className="issueActivity_statusIcon flex ">
+                    <img src={OPEN_BOX} className="" />
+                </div>
+                <p className="flex pl-5">
+                    {activity.username} start issue with{' '}
+                    {type[activity.type - 1]}{' '}
+                    {typeValue[activity.type - 1][activity.nextState]}
+                    {activity.create_time}
+                </p>
+            </div>
+        );
+    }
     return (
-      <div className="issueActivity text-16 mt-3 mb-3">
-        {activity.username} start issue with {type[activity.type - 1]}{" "}
-        {typeValue[activity.type - 1][activity.nextState]}
-      </div>
+        <div className="issueActivity text-16 mt-3 mb-3">
+            {activity.username} changed {type[activity.type - 1]} from{' '}
+            {typeValue[activity.type - 1][activity.prevState]} to{' '}
+            {typeValue[activity.type - 1][activity.nextState]}
+        </div>
     );
-  }
-  return (
-    <div className="issueActivity text-16 mt-3 mb-3">
-      {activity.username} changed {type[activity.type - 1]} from{" "}
-      {typeValue[activity.type - 1][activity.prevState]} to{" "}
-      {typeValue[activity.type - 1][activity.nextState]}
-    </div>
-  );
 };
 
 export default IssueActivity;
--- src/components/IssueDetail/index.js
+++ src/components/IssueDetail/index.js
@@ -1,173 +1,181 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-import Layout from "../Layout";
-import CardLayout from "../CardLayout";
-import IssueAPI from "../../utils/api/apifetcher/issue";
+import Layout from '../Layout';
+import CardLayout from '../CardLayout';
+import IssueAPI from '../../utils/api/apifetcher/issue';
 
-import LoadingSpinner from "../LoadingSpinner";
-import IssueActivity from "../IssueActivity";
-import IssueActivityForm from "../IssueActivityForm";
-import EditIssueForm from "../EditIssueForm";
-import PopUp from "../PopUp";
+import LoadingSpinner from '../LoadingSpinner';
+import IssueActivity from '../IssueActivity';
+import IssueActivityForm from '../IssueActivityForm';
+import EditIssueForm from '../EditIssueForm';
+import PopUp from '../PopUp';
 
-import { red_alert } from "../../utils/configConst";
-import "./style.scss";
-import MARK_ICON from "../../utils/image/exclamation-mark.svg";
+import { red_alert } from '../../utils/configConst';
+import './style.scss';
+import MARK_ICON from '../../utils/image/exclamation-mark.svg';
 
 import {
-  STATE_OPTION,
-  SEVERITY_OPTION,
-  PRIORITY_OPTION
-} from "../../utils/configConst";
+    STATE_OPTION,
+    SEVERITY_OPTION,
+    PRIORITY_OPTION
+} from '../../utils/configConst';
 
 const IssueDetail = () => {
-  const { issueId } = useParams();
-  const [issueDetail, setIssueDetail] = useState(null);
-  const [fetchingError, setFetchingError] = useState(null);
+    const { issueId } = useParams();
+    const [issueDetail, setIssueDetail] = useState(null);
+    const [fetchingError, setFetchingError] = useState(null);
 
-  const [showEditIssue, setEditIssue] = useState(false);
-  const [editErrorMsg, setErrorMsg] = useState(null);
+    const [showEditIssue, setEditIssue] = useState(false);
+    const [editErrorMsg, setErrorMsg] = useState(null);
 
-  const { username } = useSelector(state => state.AuthReducer);
+    const { username } = useSelector(state => state.AuthReducer);
 
-  useEffect(() => {
-    if (issueDetail != null) setIssueDetail(null);
-    IssueAPI.getIssueByIssueId(issueId).then(
-      res => {
-        setIssueDetail(res.data);
-        console.log("Here is the issue detail", res.data);
-      },
-      rej => {
-        const { status } = rej.response;
-        if (status === 404) setFetchingError(red_alert.NOT_FOUND);
-        else setFetchingError(red_alert.TRY_AGAIN_LATER);
-      }
-    );
-  }, [issueId]);
+    useEffect(() => {
+        if (issueDetail != null) setIssueDetail(null);
+        IssueAPI.getIssueByIssueId(issueId).then(
+            res => {
+                setIssueDetail(res.data);
+                console.log('Here is the issue detail', res.data);
+            },
+            rej => {
+                const { status } = rej.response;
+                if (status === 404) setFetchingError(red_alert.NOT_FOUND);
+                else setFetchingError(red_alert.TRY_AGAIN_LATER);
+            }
+        );
+    }, [issueId]);
 
-  const handleAddIssueActivity = query => {
-    IssueAPI.addIssueActivity(query).then(
-      res => {
-        setIssueDetail(res.data);
-        console.log("Here is the issue detail", res.data);
-      },
-      rej => {
-        const { status } = rej.response;
-        if (status === 404) setFetchingError(red_alert.NOT_FOUND);
-        else setFetchingError(red_alert.TRY_AGAIN_LATER);
-      }
-    );
-  };
+    const handleAddIssueActivity = query => {
+        IssueAPI.addIssueActivity(query).then(
+            res => {
+                setIssueDetail(res.data);
+                console.log('Here is the issue detail', res.data);
+            },
+            rej => {
+                const { status } = rej.response;
+                if (status === 404) setFetchingError(red_alert.NOT_FOUND);
+                else setFetchingError(red_alert.TRY_AGAIN_LATER);
+            }
+        );
+    };
 
-  const handleEditSubmit = query => {
-    IssueAPI.editIssue(query).then(
-      res => {
-        setIssueDetail(res.data);
-        setEditIssue(false);
-        setErrorMsg(null);
-        console.log("Here is the issue detail", res.data);
-      },
-      rej => {
-        setErrorMsg(red_alert.TRY_AGAIN_LATER);
-      }
-    );
-  };
-  const renderIssueDetail = () => {
-    return (
-      <CardLayout>
-        <div className="issueDetail">
-          <div className="flex justify-between">
-            <div className="flex">
-              <img src={MARK_ICON} className="issueDetail_icon flex " />
-              <div className="flex font-bold">{issueDetail.name}</div>
+    const handleEditSubmit = query => {
+        IssueAPI.editIssue(query).then(
+            res => {
+                setIssueDetail(res.data);
+                setEditIssue(false);
+                setErrorMsg(null);
+                console.log('Here is the issue detail', res.data);
+            },
+            rej => {
+                setErrorMsg(red_alert.TRY_AGAIN_LATER);
+            }
+        );
+    };
+    const renderIssueDetail = () => {
+        return (
+            <CardLayout>
+                <div className="issueDetail">
+                    <div className="flex justify-between">
+                        <div className="flex">
+                            <img
+                                src={MARK_ICON}
+                                className="issueDetail_icon flex "
+                            />
+                            <div className="flex font-bold">
+                                {issueDetail.name}
+                            </div>
+                        </div>
+                        {issueDetail.reportUser === username ? (
+                            <div
+                                className="main_btn btn-active"
+                                onClick={() => {
+                                    setEditIssue(!showEditIssue);
+                                }}
+                            >
+                                Edit Issue
+                            </div>
+                        ) : null}
+                    </div>
+                    <div className="hl" />
+                    <div>
+                        <div className="font-bold">Description</div>
+                        <br />
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: issueDetail.description
+                            }}
+                        />
+                    </div>
+                    <div className="hl" />
+                    <div className="issueDetail_state">
+                        <div>
+                            Issue state:{' '}
+                            <b>{STATE_OPTION[issueDetail.state]}</b>
+                        </div>
+                        <div>
+                            Issue severity:{' '}
+                            <b>{PRIORITY_OPTION[issueDetail.priority]}</b>
+                        </div>
+                        <div>
+                            Issue priority:{' '}
+                            <b>{SEVERITY_OPTION[issueDetail.projectId]}</b>
+                        </div>
+                    </div>
+                </div>
+            </CardLayout>
+        );
+    };
+
+    const renderIssueActivity = () => {
+        const { activity } = issueDetail;
+        return (
+            <div className="issueDetail_activity ">
+                {activity.map((activity, i) => (
+                    <IssueActivity
+                        key={`issue_activity${i}${activity.id}`}
+                        activity={activity}
+                    />
+                ))}
             </div>
-            {issueDetail.reportUser === username ? (
-              <div
-                className="main_btn btn-active"
-                onClick={() => {
-                  setEditIssue(!showEditIssue);
-                }}
-              >
-                Edit Issue
-              </div>
+        );
+    };
+
+    return (
+        <div className="issueDetail_container w-full bg-gray-200">
+            {fetchingError ? (
+                fetchingError
+            ) : issueDetail ? (
+                <React.Fragment>
+                    {renderIssueDetail()}
+                    {renderIssueActivity()}
+                    <CardLayout>
+                        <IssueActivityForm
+                            issueDetail={issueDetail}
+                            handleSubmit={handleAddIssueActivity}
+                        />
+                    </CardLayout>
+                </React.Fragment>
+            ) : (
+                <LoadingSpinner />
+            )}
+            {showEditIssue ? (
+                <PopUp>
+                    <EditIssueForm
+                        issueDetail={issueDetail}
+                        errorMsg={editErrorMsg}
+                        handleSubmit={handleEditSubmit}
+                        handleCancel={() => {
+                            setErrorMsg(null);
+                            setEditIssue(false);
+                        }}
+                    />
+                </PopUp>
             ) : null}
-          </div>
-          <div className="hl" />
-          <div>
-            <div className="font-bold">Description</div>
-            <br />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: issueDetail.description
-              }}
-            />
-          </div>
-          <div className="hl" />
-          <div className="issueDetail_state">
-            <div>
-              Issue state: <b>{STATE_OPTION[issueDetail.state]}</b>
-            </div>
-            <div>
-              Issue severity: <b>{PRIORITY_OPTION[issueDetail.priority]}</b>
-            </div>
-            <div>
-              Issue priority: <b>{SEVERITY_OPTION[issueDetail.projectId]}</b>
-            </div>
-          </div>
         </div>
-      </CardLayout>
     );
-  };
-
-  const renderIssueActivity = () => {
-    const { activity } = issueDetail;
-    return (
-      <div className="issueDetail_activity">
-        {activity.map((activity, i) => (
-          <IssueActivity
-            key={`issue_activity${i}${activity.id}`}
-            activity={activity}
-          />
-        ))}
-      </div>
-    );
-  };
-
-  return (
-    <div className="issueDetail_container w-full bg-gray-200">
-      {fetchingError ? (
-        fetchingError
-      ) : issueDetail ? (
-        <React.Fragment>
-          {renderIssueDetail()}
-          {renderIssueActivity()}
-          <CardLayout>
-            <IssueActivityForm
-              issueDetail={issueDetail}
-              handleSubmit={handleAddIssueActivity}
-            />
-          </CardLayout>
-        </React.Fragment>
-      ) : (
-        <LoadingSpinner />
-      )}
-      {showEditIssue ? (
-        <PopUp>
-          <EditIssueForm
-            issueDetail={issueDetail}
-            errorMsg={editErrorMsg}
-            handleSubmit={handleEditSubmit}
-            handleCancel={() => {
-              setErrorMsg(null);
-              setEditIssue(false);
-            }}
-          />
-        </PopUp>
-      ) : null}
-    </div>
-  );
 };
 
 export default IssueDetail;
