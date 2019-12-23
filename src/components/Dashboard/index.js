d2c7648704c72c0cd4e59420459a25d3ac3fa327
TingNgs <t105590049@ntut.org.tw>
2019-12-23 23:54:50 +0800

    Dashboard add issue list assiged

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,31 +1,70 @@
-import React, { useState } from 'react';
-import CardLayout from '../CardLayout';
-import Layout from '../Layout';
-import './style.scss';
+import React, { useState, useEffect } from "react";
+
+import CardLayout from "../CardLayout";
+import LoadingSpinner from "../LoadingSpinner";
+import IssueCard from "../IssueCard";
+
+import IssueApi from "../../utils/api/apifetcher/issue";
+import "./style.scss";
+
 const Dashboard = () => {
-    return (
-        <div className="dashboard w-full bg-gray-200">
-            <CardLayout>
-                <div className="dashboard_title text-20 font-semibold flex">
-                    Welcome to ITS
-                </div>
-                <div className="hl"></div>
-                <div className="dashboard_content text-18 flex">
-                    An issue tracking system (also ITS, trouble ticket system,
-                    support ticket, request management or incident ticket
-                    system) is a computer software package that manages and
-                    maintains lists of issues.[1] Issue tracking systems are
-                    generally used in collaborative settings—especially in large
-                    or distributed collaborations—but can also be employed by
-                    individuals as part of a time management or personal
-                    productivity regime. These systems often encompass resource
-                    allocation, time accounting, priority management, and
-                    oversight workflow in addition to implementing a centralized
-                    issue registry.
-                </div>
-            </CardLayout>
-        </div>
+  const [issueList, setIssueList] = useState(null);
+
+  useEffect(() => {
+    const query = {
+      timestamp: null,
+      user_id: localStorage.getItem("profileId"),
+      limit: 999,
+      isOwner: true
+    };
+    IssueApi.getIssueByUserId(query).then(
+      res => {
+        const { data } = res;
+        setIssueList(data);
+      },
+      rej => {
+        setIssueList([]);
+      }
     );
+  }, []);
+
+  return (
+    <div className="dashboard w-full bg-gray-200 flex">
+      <div className="w-full md:w-1/2 lg:w-2/3">
+        <CardLayout>
+          <div className="dashboard_title text-20 font-semibold flex">
+            Welcome to ITS
+          </div>
+          <div className="hl"></div>
+          <div className="dashboard_content text-18 flex">
+            An issue tracking system (also ITS, trouble ticket system, support
+            ticket, request management or incident ticket system) is a computer
+            software package that manages and maintains lists of issues.[1]
+            Issue tracking systems are generally used in collaborative
+            settings—especially in large or distributed collaborations—but can
+            also be employed by individuals as part of a time management or
+            personal productivity regime. These systems often encompass resource
+            allocation, time accounting, priority management, and oversight
+            workflow in addition to implementing a centralized issue registry.
+          </div>
+        </CardLayout>
+        <CardLayout>
+          <div>Issue assiged to you:</div>
+          {issueList === null ? (
+            <LoadingSpinner />
+          ) : (
+            issueList.map((e, i) => {
+              return (
+                <div key={`dashboard_issueList${i}${e.id}`}>
+                  <IssueCard key={`pit${e.id}`} issue={e} />
+                </div>
+              );
+            })
+          )}
+        </CardLayout>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
