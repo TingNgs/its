1a07ecbdc4e2ea8281c6a9a475cd7d9d55e1f90f
TingNgs <t105590049@ntut.org.tw>
2019-12-24 01:10:20 +0800

    Add get issue by tag in projectDetail tag tab

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -39,8 +39,8 @@ const Dashboard = () => {
           <div className="dashboard_content text-18 flex">
             An issue tracking system (also ITS, trouble ticket system, support
             ticket, request management or incident ticket system) is a computer
-            software package that manages and maintains lists of issues.[1]
-            Issue tracking systems are generally used in collaborative
+            software package that manages and maintains lists of issues. Issue
+            tracking systems are generally used in collaborative
             settings—especially in large or distributed collaborations—but can
             also be employed by individuals as part of a time management or
             personal productivity regime. These systems often encompass resource
--- src/containers/ProjectDetail/TagTab/index.js
+++ src/containers/ProjectDetail/TagTab/index.js
@@ -1,45 +1,116 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useDispatch, useSelector, useStore } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useLocation } from "react-router-dom";
+import { useDispatch, useSelector, useStore } from "react-redux";
 
-import IssueCard from '../../../components/IssueCard';
-import LoadingSpinner from '../../../components/LoadingSpinner';
+import IssueCard from "../../../components/IssueCard";
+import LoadingSpinner from "../../../components/LoadingSpinner";
+import IssueApi from "../../../utils/api/apifetcher/issue";
 
-import { CONST } from '../constants';
-import { HIT_BOTTOM } from '../../../utils/configConst';
-import * as actions from '../actions';
-import './style.scss';
+import { CONST } from "../constants";
+import { HIT_BOTTOM } from "../../../utils/configConst";
+import * as actions from "../actions";
+import "./style.scss";
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
 
 const TagTab = ({}) => {
-    const dispatch = useDispatch();
-    const { projectDetail, projectTagList, isFetchingProjectTag } = useSelector(
-        state => state.ProjectDetailReducer
-    );
+  const dispatch = useDispatch();
+  const { projectDetail, projectTagList, isFetchingProjectTag } = useSelector(
+    state => state.ProjectDetailReducer
+  );
+  const [issueList, setIssueList] = useState(null);
 
-    useEffect(() => {
-        actions.fetchProjectTag(projectDetail.id)(dispatch);
-    }, []);
+  const tag = useQuery().get("tag");
+  const { user, project } = useParams();
 
+  useEffect(() => {
+    actions.fetchProjectTag(projectDetail.id)(dispatch);
+  }, []);
+
+  useEffect(() => {
+    if (tag === null) {
+      setIssueList(null);
+    } else {
+      IssueApi.getIssueByProjectId({
+        projectId: projectDetail.id,
+        tag
+      }).then(res => {
+        setIssueList(res.data);
+      });
+    }
+  }, [tag]);
+
+  const renderTagIssueList = () => {
     return (
-        <div className="TagTab_container w-full">
-            <div className="TagTab_title text-20 font-semibold">
-                {CONST.tagTab_title}
+      <div>
+        <div className="flex">
+          <Link to={`/p/${user}/${project}?tab=tag`}>
+            <div
+              key={`TagTab_Tag_back`}
+              className="TagTab_Tags_container bg-green-200 cursor-pointer"
+            >
+              {"<- Show all tag"}
             </div>
-            <div className="TagTab_Tags text-12 ">
-                <span>
-                    {projectTagList.map(e => (
-                        <div
-                            key={`TagTab_Tag${e}`}
-                            className="TagTab_Tags_container bg-green-200"
-                        >
-                            {e}
-                        </div>
-                    ))}
-                    {isFetchingProjectTag ? <LoadingSpinner /> : null}
-                </span>
+          </Link>
+          <Link to={`/p/${user}/${project}?tab=tag&tag=${tag}`}>
+            <div
+              key={`TagTab_Tag${tag}`}
+              className="TagTab_Tags_container bg-green-200 cursor-pointer"
+            >
+              {tag}
             </div>
+          </Link>
         </div>
+        <div>
+          {issueList == null ? (
+            <LoadingSpinner />
+          ) : (
+            <>
+              {issueList.map((e, i) => {
+                return (
+                  <div key={`projectDetailTag_issueList${i}${e.id}`}>
+                    <IssueCard key={`pit${e.id}`} issue={e} />
+                  </div>
+                );
+              })}
+              {HIT_BOTTOM}
+            </>
+          )}
+        </div>
+      </div>
     );
+  };
+
+  const renderTagList = () => {
+    return (
+      <div>
+        {projectTagList.map(e => (
+          <Link to={`/p/${user}/${project}?tab=tag&tag=${e}`}>
+            <div
+              key={`TagTab_Tag${e}`}
+              className="TagTab_Tags_container bg-green-200 cursor-pointer"
+            >
+              {e}
+            </div>
+          </Link>
+        ))}
+        {isFetchingProjectTag ? <LoadingSpinner /> : null}
+      </div>
+    );
+  };
+
+  return (
+    <div className="TagTab_container w-full">
+      <div className="TagTab_title text-20 font-semibold">
+        {CONST.tagTab_title}
+      </div>
+      <div className="TagTab_Tags text-12 ">
+        {tag ? renderTagIssueList() : renderTagList()}
+      </div>
+    </div>
+  );
 };
 
 export default TagTab;
