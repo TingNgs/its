e4ddd0251891f7ddb80080bbe6bc4f5537d81970
TingNgs <t105590049@ntut.org.tw>
2019-12-23 23:17:16 +0800

    Add report day option and priority option

--- src/containers/ProjectDetail/ReportTab/index.js
+++ src/containers/ProjectDetail/ReportTab/index.js
@@ -4,16 +4,24 @@ import { CONST } from "../constants";
 import { toLocalTime } from "../../../utils/generalUtils";
 
 import LineChart from "../../../components/LineChart";
+import FormInput from "../../../components/FormInput";
 
-import { red_alert } from "../../../utils/configConst";
+import {
+  red_alert,
+  inputType,
+  PRIORITY_OPTION
+} from "../../../utils/configConst";
 import ProjectApi from "../../../utils/api/apifetcher/project";
 
 import * as actions from "../actions";
 
 const ReportTab = ({ projectDetail }) => {
-  const [day, setDay] = useState(7);
+  const [day, setDay] = useState(0);
+  const [priority, setPriority] = useState(0);
   const [lables, setLables] = useState([]);
   const [dataset, setDataset] = useState([]);
+  const dayList = [7, 10, 31];
+  const priorityList = [-1, 0, 1, 2, 3, 4, 5];
   const LastDays = daysCount => {
     var result = [];
     for (var i = 0; i < daysCount; i++) {
@@ -24,12 +32,13 @@ const ReportTab = ({ projectDetail }) => {
 
     return result;
   };
-  const days = LastDays(day).reverse();
+
   const getReport = closed => {
+    const days = LastDays(dayList[day]).reverse();
     return ProjectApi.getProjectReport({
       projectId: projectDetail.id,
-      day,
-      priority: -1,
+      day: dayList[day],
+      priority: priorityList[priority],
       closed
     }).then(res => {
       const tempDataSet = [];
@@ -46,6 +55,7 @@ const ReportTab = ({ projectDetail }) => {
   };
 
   useEffect(() => {
+    const days = LastDays(dayList[day]).reverse();
     const getData = async () => {
       const newDataSet = [];
       newDataSet.push({
@@ -62,10 +72,42 @@ const ReportTab = ({ projectDetail }) => {
     };
     setLables(days);
     getData();
-  }, []);
+  }, [day, priority]);
+  const handleInput = e => {
+    const { name } = e.target;
+    const { value } = e.target;
+    switch (name) {
+      case "day":
+        setDay(value);
+        break;
+      case "priority":
+        setPriority(value);
+        break;
+      default:
+        break;
+    }
+  };
+  const inputList = [
+    {
+      name: "day",
+      title: "Days",
+      value: day,
+      option: dayList,
+      inputType: inputType.radio
+    },
+    {
+      name: "priority",
+      title: "Priority",
+      value: priority,
+      option: ["All", ...PRIORITY_OPTION],
+      inputType: inputType.radio
+    }
+  ];
+
   return (
     <div className="w-full">
       <div className="w-full md:w-3/4 lg:w-1/2 mx-auto">
+        <FormInput inputList={inputList} handleInput={handleInput} />
         <LineChart labels={lables} dataset={dataset} />
       </div>
     </div>
